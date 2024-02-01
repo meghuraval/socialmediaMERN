1a288280f52c8e8e695d7219ca83c1e3d5b74ac7
meghu <meghuraval1234@gmail.com>
2024-01-31 20:50:24 -0500

    added signinpage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,11 @@ import Account from "./pages/Account";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/SIgnup";
-import Signin from "./pages/SIgnin";
 import NavBar from "./components/NavBar";
 import NavBar0 from "./components/NavBar0";
 import { useState } from "react";
+//import Signin from "./pages/SIgnin";
+import SigninPage from "./pages/SigninPage";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -19,7 +20,7 @@ function App() {
         <Route path="/homepage" Component={Homepage} />
         <Route
           path="/signin"
-          element={<Signin setAuthenticated={setAuthenticated} />}
+          element={<SigninPage setAuthenticated={setAuthenticated} />}
         />
         <Route path="/signup" Component={Signup} />
         <Route
---
+++ frontend/src/pages/SigninPage.jsx
@@ -0,0 +1,121 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithGooglePopup } from "../firebase";
+
+// eslint-disable-next-line react/prop-types
+const SigninPage = ({ setAuthenticated }) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSignIn = async () => {
+    try {
+      const response = await fetch(
+        "https://notes-mern-app-aoww.onrender.com/user/signin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      localStorage.setItem("jwtToken", data.token);
+      localStorage.setItem("userId", data.userId);
+
+      console.log("User ID", data.userId);
+      console.log("Sign-in successful:", data);
+      setAuthenticated(true);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Error signing in:", error.message);
+    }
+  };
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      // Call the signInWithGooglePopup function
+      const userCredential = await signInWithGooglePopup();
+
+      // Access the signed-in user
+      const user = userCredential.user;
+
+      // Save user information to localStorage or perform other actions
+      localStorage.setItem("jwtToken", user.accessToken);
+      localStorage.setItem("userId", user.uid);
+
+      console.log("User ID", user.uid);
+      console.log("Google Sign-in successful:", user);
+      setAuthenticated(true);
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center flex-col content-center items-center">
+      <p className="text-4xl py-5 font-semibold">Welcome Back!</p>
+      <form
+        encType="multipart/form-data"
+        className="flex flex-col items-center pt-[5dvh] border-[2px] border-gray-400 w-[80dvw] rounded-2xl"
+      >
+        <label htmlFor="email">Email:</label>
+        <input
+          className="border border-black"
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          className="border border-black"
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <br />
+
+        <button
+          type="button"
+          className="py-3 bg-blue-300 px-5 mb-5 rounded-lg border-blue-400 border-[2px] shadow-xl hover:scale-105 duration-300"
+          onClick={handleSignIn}
+        >
+          Sign In
+        </button>
+      </form>
+      <div>
+        <p>Dummy Email: testaccount@gmail.com</p>
+        <p>Dummy Password: testaccount</p>
+      </div>
+    </div>
+  );
+};
+
+export default SigninPage;
